ad6b7c2bc7157a628af391fd20ef370d8026f877
Subs <BytesWarning@gmail.com>
2025-11-20 00:32:47 +1300

    Issue #master - Initialize the project.

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -153,9 +153,9 @@ export const Contact = (props) => {
       <div id="footer">
         <div className="container text-center">
           <p>
-            &copy; 2023 Issaaf Kattan React Land Page Template. Design by{" "}
-            <a href="http://www.templatewire.com" rel="nofollow">
-              TemplateWire
+            &copy; 2025 New Epoch LTD. Design by{" "}
+            <a href="http://www.google.com" rel="nofollow">
+              New Epoch
             </a>
           </p>
         </div>
